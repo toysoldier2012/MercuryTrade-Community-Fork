c17a9210f5a8fa57bd9796a17ea04ffbe247b72d
Morph21 <krzysztof.gr@hotmail.com>
2022-04-11 21:58:40 +0200

    Automated build scripts added

---
+++ build_mercury.bat
@@ -0,0 +1,13 @@
+echo off
+echo MercuryTrade build starting
+echo mvn clean package
+call mvn clean package
+echo Copying MercuryTrade.jar from app/target to release_files
+cd app/target
+copy MercuryTrade.jar "../../release_files"
+cd ../..
+echo Launching launch4j.exe to generate MercuryTrade.exe from .jar file
+cd launch4j
+launch4jc.exe ../release_files/release_config.xml
+cd ..
+echo build_mercury_bat completed
---
+++ build_mercury_with_zip.bat
@@ -0,0 +1,30 @@
+echo off
+cls
+echo MercuryTrade build starting
+echo mvn clean package
+call mvn clean package
+echo Copying MercuryTrade.jar from app/target to release_files
+cd app/target
+copy MercuryTrade.jar "../../release_files"
+cd ../..
+echo Launching launch4j.exe to generate MercuryTrade.exe from .jar file
+cd launch4j
+launch4jc.exe ../release_files/release_config.xml
+cd ..
+echo build_mercury_bat completed
+
+echo preparing zip files for release starting...
+echo zipping with jar file start
+cd release_files
+copy MercuryTrade.jar MercuryTrade
+call powershell Compress-Archive MercuryTrade MercuryTrade.jar.zip
+cd MercuryTrade
+del MercuryTrade.jar
+cd ..
+echo Zipping with jar completed
+echo Zipping with exe file start
+copy MercuryTrade.exe MercuryTrade
+call powershell Compress-Archive MercuryTrade MercuryTrade.exe.zip
+cd MercuryTrade
+del MercuryTrade.jar
+echo zipping with exe completed
